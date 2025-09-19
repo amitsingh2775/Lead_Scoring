9d481bd2ded0a465d08932451a7cdff50e19231e
amit kumar <amitsinghbadram@gmail.com>
2025-09-19 11:57:59 +0530

    upload controller implemented

--- controllers/lead.controllers.js
+++ controllers/lead.controllers.js
@@ -1,5 +1,5 @@
 const db=require("../storage/memory")
-
+const { Readable } = require('stream');
 
 // create the set offer controller
 
@@ -11,6 +11,24 @@ exports.setOffers=(req,res)=>{
   db.offer = req.body;
   res.status(201).json({ message: 'Offer details saved.' });
     } catch (error) {
-        res.status(500).json({ message: 'server side error' });
+        return res.status(500).json({ message: 'server side error' });
     }
 }
+
+// uploads lead controllers
+exports.uploadLeads = (req, res) => {
+try {
+      if (!req.file) {
+        return res.status(400).json({ error: 'No CSV file uploaded.' });
+      }
+    
+      db.leads = []; // Clear previous leads
+      const stream = Readable.from(req.file.buffer.toString());
+    
+      stream.pipe(csv())
+        .on('data', (row) => db.leads.push(row))
+        .on('end', () => res.status(200).json({ message: `${db.leads.length} leads uploaded.` }));
+} catch (error) {
+    return res.status(500).json({ message: 'server side error' });
+}
+};
